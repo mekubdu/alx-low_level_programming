f0f8c3f33124a9003aa4a4ce27abf860bc946bab
meku <mekubdu@gmail.com>
2022-12-09 04:52:55 -0800

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - prints "with proper grammer, but the outcome is 
  * a line of art," followed newline 
  * return always 0.
- *
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
