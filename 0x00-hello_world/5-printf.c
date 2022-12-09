9e5e4556a7d319df67d18882a43810c879529ae3
meku <mekubdu@gmail.com>
2022-12-09 10:39:14 -0800

    Task 5.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints "with proper grammer" followed newline 
+ * main - prints followed newline 
  * return: always 0 (success)
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
 	return (0);
 }
