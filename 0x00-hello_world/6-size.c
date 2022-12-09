1f06d5d19fe586029ad4986ff9c329f26aba4533
meku <mekubdu@gmail.com>
2022-12-09 11:29:01 -0800

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
+#include <stdio.h>                   
 
-/**
- * main - Entry point
+/**                                                               
  *
+ * main - Entry point 
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(void)                                                    
 {
 	int a;
 	long int b;
 	long long int c;
 	char d;
-	float f;
+	float f;                                                  
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
