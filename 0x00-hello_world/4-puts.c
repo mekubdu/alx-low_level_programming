adf310abb88f8b977db36e6145db36dd419676e9
meku <mekubdu@gmail.com>
2022-12-09 04:13:07 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - Entry point 
+ * main - Entry point
+ *  
  * return: Always 0 (Success)
+ *
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+
+}
