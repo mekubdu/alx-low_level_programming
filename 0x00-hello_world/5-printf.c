518abe3099a57d1c31d69c1f94eba7581651529f
meku <mekubdu@gmail.com>
2022-12-12 07:13:12 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
  *
- * main - prints followed newline
- *
- * return: always 0 (success)
- *
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
