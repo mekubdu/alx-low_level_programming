0342369787027ebb05906f075f183e2566f27a87
meku <mekubdu@gmail.com>
2022-12-13 02:20:22 -0800

    putchar printed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - rpints putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puchchar("_");
+	_puchchar("p");
+	_puchchar("u");
+	_puchchar("t");
+	_puchchar("c");
+	_puchchar("h");
+	_puchchar("a");
+	_puchchar("r");
+	_puchchar("\n");
+}
