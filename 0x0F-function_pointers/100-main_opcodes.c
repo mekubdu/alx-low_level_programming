74aea92962143cbeac8fa74dcc779caf06e6fb90
meku <mekubdu@gmail.com>
2023-01-18 09:01:52 -0800

    Updated

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 /**
-* main - number of bytes
-*@argc: argument counter
-*@argv: argument vector
-*Return: 0
-**/
-
+ * main - prints opcode of own main function
+ *
+ * @argc: argument count
+ * @argv: arg value
+ * Return: int
+ */
 int main(int argc, char *argv[])
 {
-	int byte;
+	int x, i;
+	unsigned char *p;
 
-	if (argc < 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
-	byte = atoi(argv[1]);
-
-	if (byte < 0)
+	x = atoi(argv[1]);
+	if (x < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
+	p = (unsigned char *)main;
+	i = 0;
+	if (x > 0)
+	{
+		while (i < (x - 1))
+			printf("%02hhx ", p[i++]);
+		printf("%hhx\n", p[i]);
+	}
 	return (0);
 }
