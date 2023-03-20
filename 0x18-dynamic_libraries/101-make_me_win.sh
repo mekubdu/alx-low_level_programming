64978cd01e86ee52a27153b03ac5197b3886f2cc
meku <mekubdu@gmail.com>
2023-03-20 06:18:34 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/meku/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD=../libhack.so
+wget -P .. https://raw.githubusercontent.com/mekubdu/alx-low_level_programming/master/0x18-dynamic_libraries/liball.so
+export LD_PRELOAD="$PWD/../liball.so"
